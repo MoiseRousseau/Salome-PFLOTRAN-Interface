277b4c1e15111db2591f2bed36b41d97c862d293
Moise Rousseau <moise.rousseau@polymtl.ca>
2019-10-10 00:06:45 -0400

    Add GUI and its animations

--- PFLOTRAN_Tools.py
+++ PFLOTRAN_Tools.py
@@ -18,18 +18,21 @@
 # Author : Moise Rousseau (2019), email at moise.rousseau@polymtl.ca
 
 
-#TODO
-# dialog box for path / ascii / region
-
 
 
 def PFLOTRANMeshExport(context):
+  from PyQt5 import QtCore
+  import sys
+  sys.path.append('/home/moise/Ecole/Doc/plugin SALOME/Salome-PFLOTRAN-Interface/')
   import importlib
   import exportMesh
   import exportSubMesh
+  import UI_PFLOTRANTools
   importlib.reload(exportMesh)
   importlib.reload(exportSubMesh)
+  importlib.reload(UI_PFLOTRANTools)
   from salome.gui import helper
+  import SMESH
   print ("\n\n\n")
   print ("##################################\n")
   print (" Pflotran mesh converter for Salome 9.2.0 \n")
@@ -37,31 +40,189 @@ def PFLOTRANMeshExport(context):
   print ("  Export Salome meshes to Pflotran  \n")
   print ("###################################\n")
   
-  def GetFolder(path):
-    l = path.split('/')
-    path = ''
-    for i in range(0,len(l)-1):
-      path = path + l[i] + '/'
-    return path
+#  def GetFolder(path):
+#    l = path.split('/')
+#    path = ''
+#    for i in range(0,len(l)-1):
+#      path = path + l[i] + '/'
+#    return path
 
   
   # get context study, studyId, salomeGui
   activeStudy = salome.myStudy
 
   #create folder for exportation
-  activeFolder = activeStudy._get_URL()
-  activeFolder = GetFolder(activeFolder)
-  print ("Mesh to be save in the folder " + activeFolder)
+#  activeFolder = activeStudy._get_URL()
+#  activeFolder = GetFolder(activeFolder)
+#  print ("Mesh to be save in the folder " + activeFolder)
+
+  class ExportDialog(QDialog):
+    
+    def __init__(self):
+      QDialog.__init__(self)
+      # Set up the user interface from Designer.
+      #self.ui = EDZ_permeability_dataset_GUI.Ui_Dialog()
+      self.ui = UI_PFLOTRANTools.Ui_Dialog()
+      self.ui.setupUi(self)
+      self.show()
+      
+      # Connect up the selectionChanged() event of the object browser.
+      #sg.getObjectBrowser().selectionChanged.connect(self.select)
+      self.selectMesh = False
+      self.selectedMesh = None
+      self.selectSubmesh = False
+      self.availableSubmesh = {}
+      self.selectedSubmesh = []
+      self.binaryOutput = True #default
+      self.format = True #implicit or false = explicit
+      self.autocomplete = False
+      
+      # Connect up the buttons.
+      self.ui.pb_origMeshFile.clicked.connect(self.setMeshInput)
+      self.ui.pb_origOutputFile.clicked.connect(self.setOutputFile)
+      self.ui.pb_help.clicked.connect(self.helpMessage)
+      self.ui.cb_enableSubmesh.clicked.connect(self.enableSubmesh)
+      self.ui.pb_addSubmesh.clicked.connect(self.addGroup)
+      self.ui.pb_removeSubmesh.clicked.connect(self.removeGroup)
+      #self.ui.pb_OutputFile.clicked.connect(self.setOutputFile)
+      #self.ui.pb_help.clicked.connect(self.helpMessage)
+      
+      #self.select()
+      
+      return
+      
+      
+    def select(self):
+      #sg.getObjectBrowser().selectionChanged.disconnect(self.select)
+      objId = salome.sg.getSelected(0)
+      if self.selectMesh:
+        self._selectMeshInput(objId)
+      return
+      
+    def _selectMeshInput(self, objId):
+      self.selectedMesh = salome.IDToObject(objId)
+      if isinstance(self.selectedMesh,salome.smesh.smeshBuilder.meshProxy):
+        name = salome.smesh.smeshBuilder.GetName(self.selectedMesh)
+        smesh = salome.smesh.smeshBuilder.New()
+        self.selectedMesh = smesh.Mesh(self.selectedMesh)
+        if not self.selectedMesh.GetElementsByType(SMESH.VOLUME):
+          self.selectedMesh = None
+          return
+#      elif isinstance(self.selectedMesh,SMESH._objref_SMESH_Group):
+#        name = salome.smesh.smeshBuilder.GetName(self.selectedMesh)
+#      elif isinstance(self.selectedMesh,salome.smesh.smeshBuilder.submeshProxy):
+#        name = salome.smesh.smeshBuilder.GetName(self.selectedMesh)
+#      else:
+#        return
+      self.ui.le_origMeshFile.setText(name)
+      return
+      
+    def setMeshInput(self):
+      if self.selectMesh == True:
+        self.selectMesh = False
+        context.sg.getObjectBrowser().selectionChanged.disconnect(self.select)
+      else:
+        self.selectMesh = True
+        context.sg.getObjectBrowser().selectionChanged.connect(self.select)
+        self.select()
+      return
+      
+    def setOutputFile(self):
+      fd = QFileDialog(self, "Select an output name", self.ui.le_origOutputFile.text(), "All Files (*)")
+      if fd.exec_():
+        self.outputFile = fd.selectedFiles()[0]
+        self.ui.le_origOutputFile.setText(self.outputFile)
+      return
+      
+    def enableSubmesh(self):
+      if self.selectSubmesh:
+        self.selectedSubmesh = []
+        self.availableSubmesh = {}
+        self.ui.table_availableMesh.setRowCount(0)
+        self.ui.table_toExport.setRowCount(0)
+        self.selectSubmesh = False
+      elif self.selectedMesh:
+        groups = []
+        for x in self.selectedMesh.GetGroups():
+          if (x.GetTypes()[0] == SMESH.VOLUME or x.GetTypes()[0] == SMESH.FACE):
+            groups.append(x)
+        self.ui.table_availableMesh.setRowCount(len(groups))
+        for i,x in enumerate(groups):
+          item = QTableWidgetItem()
+          item.setText(x.GetName())
+          self.ui.table_availableMesh.setItem(i,0, item)
+          self.ui.table_availableMesh.itemChanged.connect(self.restaureGroupName)
+          self.availableSubmesh[item.text()] = x
+        self.selectSubmesh = True
+      return
+      
+    def restaureGroupName(self):
+      print('todo')
+      #TODO
+      return
+      
+    def addGroup(self):
+      self.exchangeTable(self.ui.table_availableMesh, self.ui.table_toExport)
+      return
+    
+    def removeGroup(self):
+      self.exchangeTable(self.ui.table_toExport, self.ui.table_availableMesh)
+      
+    def exchangeTable(self,dep,fin):
+      itemsSelected = dep.selectedItems()
+      itemsSelectedIndex = dep.selectedIndexes()
+      n = fin.rowCount()
+      fin.setRowCount(n+len(itemsSelected))
+      for i,x in enumerate(itemsSelected):
+        group = self.availableSubmesh[x.text()]
+        item = QTableWidgetItem()
+        item.setText(group.GetName())
+        fin.setItem(n+i,0, item)
+        fin.setItem(n+i,1, QTableWidgetItem(group.GetName()))
+        if group.GetTypes()[0] == SMESH.VOLUME:
+          fin.setItem(n+i,2, QTableWidgetItem('VOLUME'))
+        elif group.GetTypes()[0] == SMESH.FACE:
+          fin.setItem(n+i,2, QTableWidgetItem('FACE'))
+        else:
+          pass
+        #self.selectedSubmesh.append(group)
+      for x in itemsSelectedIndex:
+        dep.removeRow(x.row())
+      return
+      
+    def removeSubmesh(self):
+      itemsSelected = self.ui.table_toExport.selectedItems()
+      n = self.ui.table_toExport.rowCount()
+      self.ui.table_toExport.setRowCount(n+len(itemsSelected))
+      for i,x in enumerate(itemsSelected):
+        group = self.availableSubmesh[x.text()]
+        item = QTableWidgetItem()
+        item.setText(group.GetName())
+        self.ui.table_toExport.setItem(n+i,0, item)
+        self.ui.table_toExport.setItem(n+i,1, QTableWidgetItem(group.GetName()))
+        if group.GetTypes()[0] == SMESH.VOLUME:
+          self.ui.table_toExport.setItem(n+i,2, QTableWidgetItem('VOLUME'))
+        elif group.GetTypes()[0] == SMESH.FACE:
+          self.ui.table_toExport.setItem(n+i,2, QTableWidgetItem('FACE'))
+        else:
+          pass
+        self.selectedSubmesh.append(group)
+      return
+      
+    def helpMessage(self):
+      import subprocess
+      subprocess.Popen(['firefox'], shell=False)
+      return
+
+
+  window = ExportDialog()
+  window.exec_()
+  result = window.result()
   
-  #Option selection
-  outputFileFormat = 2 #1:ASCII or 2:HDF5
-  outputMeshFormat = 1 #1:Implicit or 2:Explicit
-  exportSubmesh = True
+  return
   
   #Export selected meshes
   print ("Export selected mesh")
-  meshToExport = helper.getSObjectSelected()[0].GetObject()
-  name = None #for future implementation of name chosen by user
   exportMesh.meshToPFLOTRAN(meshToExport, activeFolder, outputFileFormat, outputMeshFormat, name)
   
   #retrieve submesh
@@ -113,7 +274,7 @@ def PFLOTRANMeshExport(context):
   return
   
   
-salome_pluginsmanager.AddFunction('PFLOTRAN Tools GUI/Export mesh to PFLOTRAN_GUI',
+salome_pluginsmanager.AddFunction('PFLOTRAN Tools GUI/Export mesh to PFLOTRAN_GUI_real',
                                   'Export mesh and submesh to PFLOTRAN readable format',
                                   PFLOTRANMeshExport)
 
---
+++ UI_PFLOTRANTools.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+# Author : Moise Rousseau (2019), email at moise.rousseau@polymtl.ca
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+
+  def setupUi(self, Dialog):
+    Dialog.setObjectName("Dialog")
+    Dialog.resize(450, 380)
+    Dialog.setSizeGripEnabled(False)
+    
+    #principal layout
+    self.gridLayout_main = QtWidgets.QGridLayout(Dialog)
+    self.gridLayout_main.setObjectName("gridLayout_main")
+    #QtWidgets.QWidget.setLayout(self.gridLayout_main)
+    
+    #mesh/output selection
+    self.gridLayout_MSO = QtWidgets.QGridLayout(Dialog)
+    self.gridLayout_MSO.setObjectName("gridLayout_MSO")
+    #mesh
+    self.label_mesh = QtWidgets.QLabel(Dialog)
+    self.label_mesh.setObjectName("label_mesh")
+    self.pb_origMeshFile = QtWidgets.QPushButton(Dialog)
+    self.pb_origMeshFile.setObjectName("origMeshFile")
+    self.pb_origMeshFile.setCheckable(True)
+    self.le_origMeshFile = QtWidgets.QLineEdit(Dialog)
+    self.le_origMeshFile.setObjectName("le_origMeshFile")
+    self.le_origMeshFile.setReadOnly(True)
+    self.gridLayout_MSO.addWidget(self.label_mesh, 0, 0)
+    self.gridLayout_MSO.addWidget(self.pb_origMeshFile, 0, 2)
+    self.gridLayout_MSO.addWidget(self.le_origMeshFile, 0, 1)
+    #ouptut
+    self.label_output = QtWidgets.QLabel(Dialog)
+    self.label_output.setObjectName("label_output")
+    self.pb_origOutputFile = QtWidgets.QPushButton(Dialog)
+    self.pb_origOutputFile.setObjectName("origOutputFile")
+    self.le_origOutputFile = QtWidgets.QLineEdit(Dialog)
+    self.le_origOutputFile.setObjectName("le_origOutputFile")
+    self.gridLayout_MSO.addWidget(self.label_output, 1, 0)
+    self.gridLayout_MSO.addWidget(self.pb_origOutputFile, 1, 2)
+    self.gridLayout_MSO.addWidget(self.le_origOutputFile, 1, 1)
+    #add layout
+    self.gridLayout_main.addLayout(self.gridLayout_MSO, 0, 0)
+    
+    #submesh export
+    self.gridLayout_submesh = QtWidgets.QGridLayout(Dialog)
+    self.gridLayout_submesh.setObjectName("gridLayout_submesh")
+    #enable
+    self.cb_enableSubmesh = QtWidgets.QCheckBox(Dialog)
+    self.cb_enableSubmesh.setObjectName("cb_enableSubmesh")
+    self.gridLayout_submesh.addWidget(self.cb_enableSubmesh, 0, 0, 2, 1)
+    self.label_enableSubmesh = QtWidgets.QLabel(Dialog)
+    #addlayout
+    self.gridLayout_main.addLayout(self.gridLayout_submesh, 1, 0)
+    #submesh_selection
+    self.gridLayout_submesh_sel= QtWidgets.QGridLayout(Dialog)
+    self.gridLayout_submesh_sel.setObjectName("gridLayout_submesh_sel")
+    #table available
+    self.table_availableMesh = QtWidgets.QTableWidget(Dialog)
+    self.table_availableMesh.setObjectName("table_available")
+    self.table_availableMesh.setColumnCount(1)
+    self.table_header_0 = QtWidgets.QTableWidgetItem()
+    self.table_availableMesh.setHorizontalHeaderItem(0,self.table_header_0)
+    self.table_availableMesh.horizontalHeader().setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+    self.gridLayout_submesh_sel.addWidget(self.table_availableMesh, 0, 0, 4, 1)
+    #add/remove submesh
+    self.pb_addSubmesh = QtWidgets.QPushButton(Dialog)
+    self.pb_addSubmesh.setObjectName("pb_addSubmesh")
+    self.pb_addSubmesh.setText("+")
+    self.pb_removeSubmesh = QtWidgets.QPushButton(Dialog)
+    self.pb_removeSubmesh.setObjectName("pb_removeSubmesh")
+    self.pb_removeSubmesh.setText("-")
+    self.gridLayout_submesh_sel.addWidget(self.pb_addSubmesh, 1, 1, 1, 1)
+    self.gridLayout_submesh_sel.addWidget(self.pb_removeSubmesh, 2, 1, 1, 1)
+    #table to export
+    self.table_toExport = QtWidgets.QTableWidget(Dialog)
+    self.table_toExport.setObjectName("table_toExport")
+    self.table_toExport.setColumnCount(3)
+    self.table_toExport.setHorizontalHeaderItem(0,QtWidgets.QTableWidgetItem("Groups to export"))
+    self.table_toExport.setHorizontalHeaderItem(1,QtWidgets.QTableWidgetItem("Name"))
+    self.table_toExport.setHorizontalHeaderItem(2,QtWidgets.QTableWidgetItem("Groups type"))
+    self.gridLayout_submesh_sel.addWidget(self.table_toExport, 0, 2, 4,1)
+    #add submesh_sel layout
+    self.gridLayout_main.addLayout(self.gridLayout_submesh_sel, 2, 0)
+    
+    #output format
+    self.gridLayout_outputFormat = QtWidgets.QGridLayout(Dialog)
+    self.gridLayout_outputFormat.setObjectName("gridLayout_submesh")
+    self.label_outputFormat = QtWidgets.QLabel(Dialog)
+    self.label_outputFormat.setObjectName("label_outputFormat")
+    self.gridLayout_outputFormat.addWidget(self.label_outputFormat, 0, 0)
+    self.rb_outputFormatGroup = QtWidgets.QButtonGroup(Dialog)
+    self.possibleOutputFormat = ['Binary (HDF5)', 'Human readable (ASCII)']
+    self.rb_outputFormat = [QtWidgets.QRadioButton(Dialog) for i in self.possibleOutputFormat]
+    for i,radio in enumerate(self.rb_outputFormat):
+      radio.setObjectName(self.possibleOutputFormat[i])
+      radio.setText(self.possibleOutputFormat[i])
+      self.rb_outputFormatGroup.addButton(radio)
+      self.gridLayout_outputFormat.addWidget(radio, 0, i+1)
+    self.rb_outputFormat[0].setChecked(True)
+    self.gridLayout_main.addLayout(self.gridLayout_outputFormat, 3, 0)
+    
+    #grid format
+    self.gridLayout_gridFormat = QtWidgets.QGridLayout(Dialog)
+    self.gridLayout_gridFormat.setObjectName("gridLayout_gridFormat")
+    self.label_gridFormat = QtWidgets.QLabel(Dialog)
+    self.label_gridFormat.setObjectName("label_gridFormat")
+    self.gridLayout_gridFormat.addWidget(self.label_gridFormat, 0, 0)
+    self.rb_gridFormatGroup = QtWidgets.QButtonGroup(Dialog)
+    self.possibleGridFormat = ['Implicit', 'Explicit']
+    self.rb_gridFormat = [QtWidgets.QRadioButton(Dialog) for i in self.possibleGridFormat]
+    for i,radio in enumerate(self.rb_gridFormat):
+      radio.setObjectName(self.possibleGridFormat[i])
+      radio.setText(self.possibleGridFormat[i])
+      self.rb_gridFormatGroup.addButton(radio)
+      self.gridLayout_gridFormat.addWidget(radio, 0, i+1)
+    self.rb_gridFormat[0].setChecked(True)
+    self.gridLayout_main.addLayout(self.gridLayout_gridFormat, 4, 0)
+    
+    #autocompletion
+    self.gridLayout_autocompletion = QtWidgets.QGridLayout(Dialog)
+    self.gridLayout_autocompletion.setObjectName("gridLayout_autocompletion")
+    #enable
+    self.cb_enableAutocompletion = QtWidgets.QCheckBox(Dialog)
+    self.cb_enableAutocompletion.setObjectName("cb_enableAutocompletion")
+    self.gridLayout_autocompletion.addWidget(self.cb_enableAutocompletion, 0, 0)
+    #addlayout
+    self.gridLayout_main.addLayout(self.gridLayout_autocompletion, 5, 0)
+    
+    #ok and cancel button
+    self.splitter = QtWidgets.QSplitter(Dialog)
+    self.splitter.setOrientation(QtCore.Qt.Horizontal)
+    self.splitter.setObjectName("splitter")
+    self.pb_help = QtWidgets.QPushButton(self.splitter)
+    self.pb_help.setObjectName("pb_help")
+    self.pb_okCancel = QtWidgets.QDialogButtonBox(self.splitter)
+    self.pb_okCancel.setOrientation(QtCore.Qt.Horizontal)
+    self.pb_okCancel.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+    self.pb_okCancel.setObjectName("pb_okCancel")
+    self.gridLayout_main.addWidget(self.splitter, 6, 0)
+    self.pb_okCancel.accepted.connect(Dialog.accept)
+    self.pb_okCancel.rejected.connect(Dialog.reject)
+      
+    self.retranslateUi(Dialog)
+    self.setDefaultValue()
+    
+
+  def retranslateUi(self, Dialog):
+    _translate = QtCore.QCoreApplication.translate
+    Dialog.setWindowTitle(_translate("Dialog", "Export Salome meshes to PFLOTRAN"))
+    #MSO
+    self.pb_origMeshFile.setText(_translate("Dialog", "Select"))
+    self.label_mesh.setText(_translate("Dialog", "Mesh to export:"))
+    self.pb_origOutputFile.setText(_translate("Dialog", "Select"))
+    self.label_output.setText(_translate("Dialog", "Destination folder and name:"))
+    #submesh export
+    self.cb_enableSubmesh.setText(_translate("Dialog", "Export Salome submeshes or groups as PFLOTRAN regions"))
+    self.table_header_0.setText(_translate("Dialog", "Available groups"))
+    self.label_outputFormat.setText(_translate("Dialog", "Output format:"))
+    self.label_gridFormat.setText(_translate("Dialog", "Grid format:"))
+    self.cb_enableAutocompletion.setText(_translate("Dialog", "Enable autocompletion of PLFOTRAN input file"))
+    
+    #help
+    self.pb_help.setText(_translate("Dialog", "Help"))
+    
+  def setDefaultValue(self):
+    pass
+
+
+
